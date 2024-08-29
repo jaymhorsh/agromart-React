644e2c8d57f40177d50b71360fed138822fd92e2
Jaji Moshood <jaji.moshood1297@gmail.com>
2024-08-29 23:58:50 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import Header from "./component/Layout/Header/Header";
 import Home from "./component/Layout/Home/Home";
+import React from 'react'
 import Product from "./component/Product/Product";
 import Footer from "./component/Layout/Footer/Footer";
 const App = function () {
